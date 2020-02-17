c5287bb4cbff515e3e6e2ee5e21f2f03951a7e80
Chill141 <60900576+Chill141@users.noreply.github.com>
2020-02-17 11:52:38 -0500

    added shooter

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConst;;
+
+
+public class ShooterSubsystem extends SubsystemBase {
+  /**
+   * Creates a new Shooter.
+   */
+
+   //Create Shooter Motor
+  public CANSparkMax shooterMotor = new CANSparkMax(ShooterConst.Shooter, MotorType.kBrushless);
+  public CANSparkMax targetMotor = new CANSparkMax(ShooterConst.Targeting, MotorType.kBrushless);
+
+
+
+  public ShooterSubsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+  public Object shoot(double slider){
+shooterMotor.set(slider);
+return slider;
+    
+  }
+}
