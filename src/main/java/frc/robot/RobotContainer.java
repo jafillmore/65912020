d865acc9fcc504397a22b3830ce2ad9dbbac8475
Little-Sammie12 <sammie2953@gmail.com>
2020-02-19 14:41:20 -0500

    codes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -40,8 +40,7 @@ public class RobotContainer {
    * The container for the robot.  Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-     Configure the button bindings
-   /configureButtonBindings();
+   configureButtonBindings();
     
     JoystickButton intakeButton = new JoystickButton(leftJoystick, Constants.intakeNumber);
     
